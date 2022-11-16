3dc2a1d2a9e81dc1377a2a28b8d39be84bd05b56
HinataKawaguchi <hinata.money@gmail.com>
2022-11-16 23:42:00 +0900

    feat:修正

--- components/RecommendUser.tsx
+++ components/RecommendUser.tsx
@@ -4,14 +4,14 @@ import Button from "./Button";
 const RecommendUser = ({ name, image }: { name: string; image: string }) => {
   return (
     <div>
-      <div className="flex justify-between items-center px-4 py-1 ">
+      <div className="flex md:justify-between items-center px-4 py-1 ">
         <div className="flex items-center">
           <div className="">
             <img src={image} alt="" className="rounded-full w-8 h-8 mr-2" />
           </div>
           <p className="text-sm">{name}</p>
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between ml-auto md:ml-0">
           <div className="items-center">
             <Button className="max-w-4 h-7  w-3 hover:bg-green-500 hover:text-white flex shadow-none">
               フォロー
