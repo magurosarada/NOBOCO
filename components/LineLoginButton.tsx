91d01bd8178637c9ffdeb90633c01084c8ebcea5
HinataKawaguchi <hinata.money@gmail.com>
2023-01-09 02:36:22 +0900

    fix:LineLoginButtonの修正

--- components/LineLoginButton.tsx
+++ components/LineLoginButton.tsx
@@ -20,7 +20,7 @@ const AuthLine = () => {
         Icon={FaLine}
         color={"#fff"}
         className=" h-12 mt-5 rounded-full w-4/6 mx-auto px-[15px]  text-white hover:border hover:border-black  flex justify-center  items-center hover:text-white   bg-[#06c755] :"
-        onClick={() => openLineLoginPage(false)}
+        onClick={() => openLineLoginPage()}
       >
         <span>LINEアカウントでログイン</span>
       </Button>
